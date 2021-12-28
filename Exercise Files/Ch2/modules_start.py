f1fc14c45a653bb74c312849451d4d3c3d20c863
nipsalvin <amwaniki.am@gmail.com>
2021-12-28 13:28:12 +0300

    update dec 28/2021

--- Exercise Files/Ch2/modules_start.py
+++ Exercise Files/Ch2/modules_start.py
@@ -5,6 +5,7 @@ import math
 print("The square root of 16 is", math.sqrt(16))
 
 # in addition to functions, some modules contain useful constants
-print("PI is ", math.sqrts)
+print("PI is ", math.pi)
 
 # try some of the math functions for yourself here:
+print(math.factorial(5))
