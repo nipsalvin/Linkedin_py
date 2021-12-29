d782090f0d6bce9f8f179ed8b31828cdc59e196b
nipsalvin <amwaniki.am@gmail.com>
2021-12-29 15:11:29 +0300

    Reading and writting files

--- Exercise Files/Ch4/files_start.py
+++ Exercise Files/Ch4/files_start.py
@@ -4,19 +4,28 @@
 
 def main():  
   # Open a file for writing and create it if it doesn't exist
+  #f = open("textfile.txt","w+") #W = write
+  #f = open("textfile.txt","a") #a = append
+  f = open("textfile.txt","r") #r = read
 
-  
   # Open the file for appending text to the end
 
 
   # write some lines of data to the file
-
-  
+  """ for i in range(10):
+    f.write("This is the line "+ str(i) + "\r\n")
+   """
   # close the file when done
-
+  #f.close()
   
   # Open the file back up and read the contents
-
+  if f.mode == 'r':
+     #contents = f.read()
+     fl = f.readlines()
+     for x in fl:
+       print(x)
+     
+     #print (contents)
     
 if __name__ == "__main__":
   main()
