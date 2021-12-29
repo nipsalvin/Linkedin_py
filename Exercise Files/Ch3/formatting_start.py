e4e175a0c4e49f55e46f473de3b286e7bcaf2536
nipsalvin <amwaniki.am@gmail.com>
2021-12-29 11:32:19 +0300

    formatting datetime

--- Exercise Files/Ch3/formatting_start.py
+++ Exercise Files/Ch3/formatting_start.py
@@ -7,20 +7,23 @@ from datetime import datetime
 def main():
   # Times and dates can be formatted using a set of predefined string
   # control codes 
-
-  
+  now = datetime.now()
+    
   #### Date Formatting ####
   
   # %y/%Y - Year, %a/%A - weekday, %b/%B - month, %d - day of month
-
+  print(now.strftime(" %y, %Y, %a, %A, %b, %B, %d, %D "))
 
   # %c - locale's date and time, %x - locale's date, %X - locale's time
-
+  print(now.strftime("Locale date and time: %c"))
+  print(now.strftime("Locale date and time: %x"))
+  print(now.strftime("Locale date and time: %X"))
 
   #### Time Formatting ####
   
   # %I/%H - 12/24 Hour, %M - minute, %S - second, %p - locale's AM/PM
+  print(now.strftime("Current time is: %I:%M:%S :%p"))
+  print(now.strftime("Current time is: %H:%M:%S"))
   
-
 if __name__ == "__main__":
   main();
