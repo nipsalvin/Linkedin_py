6548555a32469c787d1d468e84e6d64993edb035
nipsalvin <amwaniki.am@gmail.com>
2021-12-31 14:09:36 +0300

    reading data from the internet

--- Exercise Files/Ch5/inetdata_start.py
+++ Exercise Files/Ch5/inetdata_start.py
@@ -2,5 +2,13 @@
 # Example file for retrieving data from the internet
 #
 
+import urllib.request  #This has classes needed for http requests
+
+def main(): #defining main function
+  webUrl = urllib.request.urlopen("http://google.com") #variable called webUrl 
+  print("result code: " + str(webUrl.getcode())) #response from the server is obtained by calling the getcode function (E.g 200, 404)
+  data = webUrl.read() #calling read function on the response
+  print (data)
+
 if __name__ == "__main__":
   main()
