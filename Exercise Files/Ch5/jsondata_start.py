3fb5119123217fd9302798e5c0b838eea4906437
nipsalvin <amwaniki.am@gmail.com>
2022-01-02 09:10:55 +0300

    json data

--- Exercise Files/Ch5/jsondata_start.py
+++ Exercise Files/Ch5/jsondata_start.py
@@ -2,24 +2,42 @@
 # Example file for parsing and processing JSON
 #
 import urllib.request 
+import json
 
 def printResults(data):
   # Use the json module to load the string data into a dictionary
-  theJSON = json.loads(data)
+  #takes arg called data(jSON String)
+  #Calls loads function on json class and assigns result to variable theJSON
+  theJSON = json.loads(data) 
   
   # now we can access the contents of the JSON like any other Python object
-
+  if "title" in theJSON["metadata"]:
+    print(theJSON["metadata"]["title"])
   
   # output the number of events, plus the magnitude and each event name  
-
+  count = theJSON["metadata"]["count"]
+  print(str(count)+"events recorded")
   
   # for each event, print the place where it occurred
-
+  for i in theJSON["features"]:
+    print(i["properties"]["place"])
+  print("_______________ \n")
 
   # print the events that only have a magnitude greater than 4
-
+  for i in theJSON["features"]:
+    if i ["properties"]["mag"] >= 4.0:
+      print ("%2.1f" % i["properties"]["mag"], i["properties"]["place"])
+    print("_______________ \n")
 
   # print only the events where at least 1 person reported feeling something
+  print("events that were felt: ")
+  for i in theJSON["features"]:
+    feltReports = i["properties"]["felt"]
+    if feltReports != None:
+      if feltReports >0:
+        print ("%2.1f" % i ["properties"]["mag"], i["properties"]["place"]
+        , " reported " + str(feltReports) + " times"
+        )
 
   
 def main():
@@ -29,9 +47,14 @@ def main():
   urlData = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson"
 
   # Open the URL and read the data
-  webUrl = urllib.request.urlopen(urlData)
-  print ("result code: " + str(webUrl.getcode()))
+  webUrl = urllib.request.urlopen(urlData)  #call url-open function on the url
+  print ("result code: " + str(webUrl.getcode())) 
   
+  if (webUrl.getcode() == 200):
+    data = webUrl.read()
+    printResults(data)
+  else:
+    print("Error Received! Cannot parse results")
 
 if __name__ == "__main__":
   main()
